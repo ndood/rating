d31cd4007ac4eb1cc4eb268750931d642d92827a
baiysh-san <baiysh.toktosunov@gmail.com>
2017-05-11 00:17:59 +0600

    Расскоментировал findByUserGroup(Group userGroup) РАБОТАЕТ!!!! РАБОТАЕТ!!!!

--- src/main/java/kg/ksucta/repository/MarkRepository.java
+++ src/main/java/kg/ksucta/repository/MarkRepository.java
@@ -4,16 +4,18 @@ import kg.ksucta.domain.Group;
 import kg.ksucta.domain.mark.Mark;
 import kg.ksucta.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface MarkRepository extends JpaRepository<Mark, Long> {
 
-    //List<Mark> findByUserGroup(Group userGroup);
+    List<Mark> findByUserGroup(Group userGroup);
     List<Mark> findBySemesterAndUser(Long semester, User user);
     List<Mark> findByUser(User user);
     List<Mark> findAll();
+
+
+
 
 
 
