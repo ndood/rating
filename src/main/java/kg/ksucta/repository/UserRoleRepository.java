c6b4c062ea9174a26ee00fdf6ea38b96b77c38a5
kb1prb13 <dastan.namazov.96@gmail.com>
2017-05-12 21:46:13 +0600

    Edit names of methods and his parameters (userID to user)

--- src/main/java/kg/ksucta/repository/UserRoleRepository.java
+++ src/main/java/kg/ksucta/repository/UserRoleRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-    List<UserRole> findByRole(String role);
-    List<UserRole> findByUserId(Long userId);
-    List<UserRole> findByUserIdAndRole(Long userId, String role);
-    List<UserRole> findAll();
+    List<UserRole> findByRole(String role);    //Можно ли из enum Role брать значения? findByRole(Role role)
     List<UserRole> findByUser(User user);
+    List<UserRole> findByUserAndRole(User user, String role);
+    List<UserRole> findAll();
+    
+    //    List<UserRole> findByUserIdAndRole(Long userId, String role);
+    //    List<UserRole> findByUserId(Long userId);
 }
