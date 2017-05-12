d1f81e5f040baddba566158f50c3e8f0731fb066
salavat <si300896@gmail.com>
2017-05-13 00:28:38 +0600

    Убрал моды доступа public из некоторых методов, так как они были избыточными

--- src/main/java/kg/ksucta/service/UserService.java
+++ src/main/java/kg/ksucta/service/UserService.java
@@ -7,10 +7,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-    public Optional<User> getByEmail(String email);
-    public Optional<User> getByUserName(String username);
-    public Optional<User> getByFirstName(String firstName);
-    public Optional<User> getByLastName(String lastName);
+    Optional<User> getByEmail(String email);
+    Optional<User> getByUserName(String username);
+    Optional<User> getByFirstName(String firstName);
+    Optional<User> getByLastName(String lastName);
     Optional<User> getByFirstNameAndLastName(String firstName, String lastName);
     List<User> getByGroup(Group group);
     List<User> getByGroup_Course(String course);
