569ac681e0b2d729dd2a11387828d7b5afe64880
kb1prb13 <dastan.namazov.96@gmail.com>
2017-05-20 11:33:01 +0600

    Edit paths in userRoleController

--- src/main/java/kg/ksucta/controller/UserRoleController.java
+++ src/main/java/kg/ksucta/controller/UserRoleController.java
@@ -1,14 +1,12 @@
 package kg.ksucta.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import kg.ksucta.domain.user.Role;
 import kg.ksucta.domain.user.User;
 import kg.ksucta.domain.user.UserRole;
 import kg.ksucta.service.UserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,39 +16,41 @@ public class UserRoleController {
     @Autowired
     UserRoleService userRoleService;
 
-    @RequestMapping(value = "/user/userRole/role/{role}", method = RequestMethod.GET)
-    public List<UserRole> getByRole(@PathVariable("role") Role role) {
-        List<UserRole> userRolesByRole = userRoleService.getByRole(role);
-        return userRolesByRole;
-    }
+//    @RequestMapping(value = "/role/{role}", method = RequestMethod.GET)
+//    public List<UserRole> getByRole(@PathVariable("role") Role role) {
+//        List<UserRole> userRolesByRole = userRoleService.getByRole(role);
+//        return userRolesByRole;
+//    }
 
-    @RequestMapping(value = "/user/userRole/user_id/{user_id}", method = RequestMethod.GET)
-    public List<UserRole> getByUser(@PathVariable("user_id")User user){
+    @RequestMapping(value = "/user/{user}", method = RequestMethod.GET)
+    public List<UserRole> getByUser(@PathVariable("user")User user){
         List<UserRole> userRolesByUser = userRoleService.getByUser(user);
         return userRolesByUser;
     }
 
-    @RequestMapping(value = "/user/userRole/userAndRole/{userAndRole", method = RequestMethod.GET)
+    @RequestMapping(value = "/userAndRole/{userAndRole", method = RequestMethod.GET)
     public List<UserRole> getByUserAndRole(@PathVariable("user") User user, @PathVariable("role") Role role){
         List<UserRole> userRolesByUserAndRole = userRoleService.getByUserAndRole(user, role);
         return userRolesByUserAndRole;
     }
-    /*@RequestMapping(method = RequestMethod.GET)
-    public List<UserRole> getByRole(@RequestParam("role") Role role) throws JsonProcessingException{
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<UserRole> getByRole(@RequestBody Role role) throws JsonProcessingException {
         List<UserRole> userRolesByRole = userRoleService.getByRole(role);
         return userRolesByRole;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<UserRole> getByUser(@RequestParam("user_id") User user) throws JsonProcessingException{
-        List<UserRole> userRolesByUser = userRoleService.getByUser(user);
-        return userRolesByUser;
-    }
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List<UserRole> getByUser(@RequestParam("user") User user) throws JsonProcessingException {
+//        List<UserRole> userRolesByUser = userRoleService.getByUser(user);
+//        return userRolesByUser;
+//    }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<UserRole> getByUserAndRole(@RequestParam("user") User user, @RequestParam("role") Role role)
-            throws JsonProcessingException{
-        List<UserRole> userRolesByUserAndRole = userRoleService.getByUserAndRole(user, role);
-        return userRolesByUserAndRole;
-    }*/
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List<UserRole> getByUserAndRole(@RequestParam("user") User user, @RequestParam("role") Role role)
+//            throws JsonProcessingException{
+//        List<UserRole> userRolesByUserAndRole = userRoleService.getByUserAndRole(user, role);
+//        return userRolesByUserAndRole;
+//    }
 }
--- src/main/java/kg/ksucta/repository/UserRoleRepository.java
+++ src/main/java/kg/ksucta/repository/UserRoleRepository.java
@@ -6,11 +6,10 @@ import kg.ksucta.domain.user.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
     List<UserRole> findByUserAndRole(User user, Role role);
-    List<UserRole> findAll();               //jpa требует только List<>, т.е. не получится Optional
+    List<UserRole> findAll();
     List<UserRole> findByRole(Role role);
     List<UserRole> findByUser(User user);
 }
